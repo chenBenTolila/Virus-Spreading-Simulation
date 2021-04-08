9bd0abe53d8f281a2d640c196b214df1f830cccd
chenBenTolila <chenbt266@gmail.com>
2021-04-08 23:11:35 +0300

    finished location package

--- aoop-Part1/src/country/RamzorColor.java
+++ aoop-Part1/src/country/RamzorColor.java
@@ -1,5 +1,20 @@
 package country;
 
 public enum RamzorColor {
-
+	GREEN(0.4),
+	YELLOW(0.6),
+	ORANGE(0.8),
+	RED(0.5);
+	
+	private RamzorColor(double value)
+	{
+		this.value = value;
+	}
+	
+	public double getValue()
+	{
+		return this.value;
+	}
+	
+	private final double value;
 }
