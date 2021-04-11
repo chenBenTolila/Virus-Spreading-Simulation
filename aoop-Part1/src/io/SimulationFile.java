89a9bedb0fbe0cd209a24be71935e8e1388d10c3
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2021-04-11 23:44:01 +0300

    update

--- aoop-Part1/src/io/SimulationFile.java
+++ aoop-Part1/src/io/SimulationFile.java
@@ -1,17 +1,22 @@
 package io;
 import java.io.*;
-import population.Person;
+import country.Settlement;
+import population.*;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class SimulationFile {
 	
 	
-	public void readFromFile() {
+	public void createMap() {
+		Scanner sc =new Scanner(System.in);
 		FileReader fr; 
+		String st;
 		try {
 			fr = new FileReader("SimulationFile.text");
-			
+			//st = fr.readLine();
+
 			
 			fr.close();
 		}
@@ -21,18 +26,27 @@ public class SimulationFile {
 	}
 	
 	
-	public Person intizializePersonAge(Settelment s)
+	public int intizializePersonAge()
 	{
 		int y;
-		int x;
+		double x;
 		Random rand = new Random();
-		y = rand.nextInt(5);  // check if y is real or integer //
+		y = rand.nextInt(5);  
 		Random r = new Random();
-		double mySample = r.nextGaussian()*deviation+mean;
-		
+		x = r.nextGaussian()*deviation+mean;
+		return (int)((5 * x) + y);
 	}
 
 	
+	public void createPeopleArrey(Settlement s, int numOfPersons) {
+		Healthy p;
+		for(int i=0; i< numOfPersons; ++i) {
+			p=new Healthy(intizializePersonAge(), s.randomLocation(), s );
+			s.addPerson(p);
+		}
+	}
+	
+	// data members
 	public static double deviation = 6;
 	public static double mean = 9;
 }
