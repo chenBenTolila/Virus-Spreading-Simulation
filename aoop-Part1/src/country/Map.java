5a1f837d991f2b30daa8b6c4b60b99b60085353e
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2021-04-12 15:04:57 +0300

    pass

--- aoop-Part1/src/country/Map.java
+++ aoop-Part1/src/country/Map.java
@@ -1,6 +1,19 @@
 package country;
 
+import population.Person;
+
 public class Map {
 	
-	private Settlement[] settlement;
+	private Settlement[] m_settlement;
+	
+	
+	public boolean addSettlement(Settlement s){
+		Settlement[] newArray = new Settlement[m_settlement.length + 1];  // check if the allocation succeed
+		int i;
+		for(i=0; i < m_settlement.length; ++i)
+			newArray[i] = m_settlement[i];
+		newArray[i] = s;  // adding p itself///
+		m_settlement = newArray;
+		return true;
+	}
 }
