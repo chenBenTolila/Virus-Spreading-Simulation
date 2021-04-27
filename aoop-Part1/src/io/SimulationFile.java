328ee2cfde01414d6f2e1110ade14657a2a3b8ea
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2021-04-27 22:51:07 +0300

    simulation file update

--- aoop-Part1/src/io/SimulationFile.java
+++ aoop-Part1/src/io/SimulationFile.java
@@ -26,40 +26,58 @@ public class SimulationFile {
 		while((line = bufferedReader.readLine()) != null) {
 			line = line.replaceAll(" ", "");
 			String[] data = line.split(";");
-			Settlement temp;
-			String name = data[1];
-			Point cord  = new Point(Integer.parseInt(data[2]), Integer.parseInt(data[3]));
-			Size sz = new Size(Integer.parseInt(data[4]), Integer.parseInt(data[5]));
-			Location loc = new Location(cord, sz);
-			int numPeople = Integer.parseInt(data[6]);
-			switch(data[0])
-			{
-			case "City":
-				temp = new City(name, loc, RamzorColor.GREEN, (int)(numPeople*maxP));
-				createPeopleArrey(temp, numPeople);  
-				map.addSettlement(temp);  // adding the settlement to the map
-				break;
-					
-			case "Kibbutz":
-				temp = new Kibbutz(name, loc, RamzorColor.GREEN, (int)(numPeople*maxP));
-				createPeopleArrey(temp, numPeople);
-				map.addSettlement(temp);  // adding the settlement to the map
-				break;
-					
-			case "Moshav":
-				temp  = new Moshav(name, loc, RamzorColor.GREEN, (int)(numPeople*maxP));
-				createPeopleArrey(temp, numPeople);
-				map.addSettlement(temp);  // adding the settlement to the map
-				break;
-					
-			default:
-				 System.out.println("type of settelment is undefined");
-				 break;
-			}
+			if(data[0]!="#") 
+				createSettlement(data, map);
 		}
-			fr.close();
-		
-}
+		fr.close();	
+		fr = new FileReader("SimulationFile.txt");
+		bufferedReader = new BufferedReader(fr);
+		while((line = bufferedReader.readLine()) != null) {
+			line = line.replaceAll(" ", "");
+			String[] data = line.split(";");
+			map.connectSettlements(data[1], data[2]);
+		}
+		fr.close();
+	}
+	
+	
+	/**
+	 * 
+	 * @param data get data of settlement
+	 * @param map get map of simulation
+	 */
+	public static void createSettlement(String[] data, Map map) {
+		Settlement temp;
+		String name = data[1];
+		Point cord  = new Point(Integer.parseInt(data[2]), Integer.parseInt(data[3]));
+		Size sz = new Size(Integer.parseInt(data[4]), Integer.parseInt(data[5]));
+		Location loc = new Location(cord, sz);
+		int numPeople = Integer.parseInt(data[6]);
+		switch(data[0])
+		{
+		case "City":
+			temp = new City(name, loc, RamzorColor.GREEN, (int)(numPeople*maxP));
+			createPeopleArrey(temp, numPeople);  
+			map.addSettlement(temp);  // adding the settlement to the map
+			break;
+				
+		case "Kibbutz":
+			temp = new Kibbutz(name, loc, RamzorColor.GREEN, (int)(numPeople*maxP));
+			createPeopleArrey(temp, numPeople);
+			map.addSettlement(temp);  // adding the settlement to the map
+			break;
+				
+		case "Moshav":
+			temp  = new Moshav(name, loc, RamzorColor.GREEN, (int)(numPeople*maxP));
+			createPeopleArrey(temp, numPeople);
+			map.addSettlement(temp);  // adding the settlement to the map
+			break;
+				
+		default:
+			 System.out.println("type of settelment is undefined");
+			 break;
+		}
+	}
 	
 	/**
 	 * the method calculates the age for a new person
