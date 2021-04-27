a99e26be7310374f79819a6e30f775b674f41e68
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2021-04-28 00:32:05 +0300

    virus update

--- aoop-Part1/src/virus/IVirus.java
+++ aoop-Part1/src/virus/IVirus.java
@@ -12,6 +12,6 @@ import population.Sick;
 public interface IVirus {
 	
 	 public double contagionProbability(Person p); // return the probability to get infected in a variant 
-	 public boolean tryToContagion(Person p1, Person p2); //  if the second person is healthy check if he got infected by the first person
+	 public boolean tryToContagion(Sick p1, Person p2); //  if the second person is healthy check if he got infected by the first person
 	 public boolean tryToKill(Sick s); // return true if the person died from the virus
 }
