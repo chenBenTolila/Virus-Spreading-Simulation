9e3975b0ef9e70504e87dac45fde7526a622b647
chenBenTolila <chenbt266@gmail.com>
2021-05-04 00:38:01 +0300

    new changes

--- aoop-Part1/src/simulation/Main.java
+++ aoop-Part1/src/simulation/Main.java
@@ -18,6 +18,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		try {
+			
 			Map m = new Map();   // create an empty map
 			MainWindow mw = new MainWindow(m);
 			while(true)
